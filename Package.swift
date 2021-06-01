70f1157d9499fee1071c301a0b27f5dec8e1301d
Tom Yu <tyu@nero.com>
2021-06-01 10:59:46 +0800

    Add examples

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "MyUtilities",
+    platforms: [.iOS(.v12)],
+    products: [
+        .library(name: "MyUtilities", targets: ["ColorExtension"]),
+    ],
+    targets: [
+        .target(name: "ColorExtension"),
+    ]
+)
---
+++ Project/SwiftPackages/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SwiftPackages
+//
+//  Created by Tom Yu on 2021/6/4.
+//
+
+import UIKit
+import MyUtilities
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        view.backgroundColor = UIColor()
+    }
+}
+
