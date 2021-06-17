bc3b37c80a816d592787c6c63be45e0e491fcc4b
Tom Yu <tyu@nero.com>
2021-06-17 14:54:52 +0800

    Update test project

--- Project/SwiftPackages/ViewController.swift
+++ Project/SwiftPackages/ViewController.swift
@@ -14,9 +14,12 @@ import GCDWebUploader
 
 class ViewController: UIViewController {
     
+    let imageView = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(RGB: 0xFF0000)
+        view.addSubview(imageView)
         
         let dog = Dog()
         print(dog)
@@ -29,6 +32,22 @@ class ViewController: UIViewController {
         
         let uploader = GCDWebUploader()
         print(uploader)
+        
+        /// Bundle
+        Bundle.allBundles.forEach {
+            print($0.bundleURL)
+        }
+        let path = Bundle.main.path(forResource: "MyUtilities_Animals", ofType: "bundle")
+        let bundle = Bundle(path: path!)
+        let poodle = UIImage(named: "poodle", in: bundle, compatibleWith: nil)
+        imageView.image = poodle
+        print(poodle)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        imageView.bounds = CGRect(x: 0, y: 0, width: 200, height: 200)
+        imageView.center = view.center
     }
 }
 
